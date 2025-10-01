bf05d1440bade8980348916da85c82da7016d74d
Aarushi <anu@Aarushis-MacBook-Pro.local>
2025-10-01 20:49:33 +0530

    Batch commit wal buffer

--- lib/db/batch.ml
+++ lib/db/batch.ml
@@ -11,11 +11,13 @@ open Utils
 exception ErrorBatchCommitted
 exception ErrorBatchRolledback
 
-type batch_record = {
+type wal_record = {
 	key      : Bytes.t;
 	value    : Bytes.t ;
-	batch_id : int64
+	batch_id : int64;
+    wal_type     : record_type
 }
+and record_type = | WalRecord | WalRecordEnd
 
 module  Inflightmap = struct
   type t = Bytes.t
@@ -29,8 +31,8 @@ module InflightMap = CCMap.Make(Inflightmap)
 
 type batch = {
 	db        :       DatabaseOp.data_store;
-	writes_in_flight  : batch_record Inflight_vector.vector;
-	mutable writes_in_flight_fast_access : batch_record InflightMap.t;
+	writes_in_flight  : wal_record Inflight_vector.vector;
+	mutable writes_in_flight_fast_access : wal_record InflightMap.t;
 	mu        :  Eio.Mutex.t;
 	committed    :    bool;
 	rolledback   :    bool;
@@ -53,10 +55,11 @@ let check_for_inflight_writes b key  =
 
     InflightMap.find_opt key b.writes_in_flight_fast_access
 
-let  store_inflight_writes b key batch_record =
-	let _ = CCVector.push b.writes_in_flight batch_record in
+let  store_inflight_writes b key wal_record =
+	let _ = CCVector.push b.writes_in_flight wal_record in
     let hash_of_key = murmurhash key (Int32.of_int (Bytes.length key)) (Int32.of_int 0) in
-    b.writes_in_flight_fast_access <- b.writes_in_flight_fast_access |> InflightMap.add (Bytes.of_string (Int32.to_string hash_of_key)) batch_record
+    b.writes_in_flight_fast_access <- b.writes_in_flight_fast_access
+                                      |> InflightMap.add (Bytes.of_string (Int32.to_string hash_of_key)) wal_record
 
 let batch b put key value =
 
@@ -74,6 +77,7 @@ let batch b put key value =
             key      = key;
             value    = value;
 	        batch_id      =   (match id with | Ok v -> v | Error _ -> failwith "Unable to get snowflake id");
+            wal_type = WalRecord
           } in
         store_inflight_writes b key wal_record
    )
@@ -89,6 +93,8 @@ let  commit b =
 		raise ErrorBatchCommitted;
 
 
+  let any_wal_record =  CCVector.get b.writes_in_flight 0 in
+  let batch_id = any_wal_record.batch_id in
   let buffer = Buffer.create 200 in
   let write_to_stdout stdout bytes =
     Eio.Buf_write.with_flow stdout @@ fun bw ->
@@ -100,6 +106,14 @@ let  commit b =
        buffer
        ) buffer b.writes_in_flight
   in
+       let bytes = (Marshal.to_bytes {
+                                       key = Bytes.empty;
+                                       value = Bytes.empty;
+                                       batch_id = batch_id;
+                                       wal_type = WalRecordEnd
+
+                                     } []) in
+       write_to_stdout (Eio.Flow.buffer_sink buffer) bytes;
   ()
   )
   )
